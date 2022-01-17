ab760011435d8c4d629554f111ffcfc1e461eff1
Ian Weston <iancweston@gmail.com>
2022-01-17 15:35:13 -0500

    set up null-ls items

--- nvim/lua/ian/lsp/null-ls.lua
+++ nvim/lua/ian/lsp/null-ls.lua
@@ -1,19 +1,34 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
--- local formatting = null_ls.builtins.formatting
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
+
+local formatting = null_ls.builtins.formatting
 -- local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-  debug = false,
-  sources = {
-    -- formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
-    -- formatting.black.with { extra_args = { "--fast" } },
-    -- formatting.stylua,
-    -- diagnostics.flake8,
-  },
+	debug = false,
+	sources = {
+    --  LUA
+		formatting.stylua,
+
+    --  PYTHON
+		formatting.black,
+		formatting.isort,
+		-- diagnostics.flake8,
+
+    --  SHELL
+    formatting.shfmt,
+    formatting.shellharden,
+    -- diagnostics.shellcheck,
+
+    -- JSON/YAML/MARKDOWN
+		formatting.prettier,
+
+    -- YAML
+    -- diagnostics.yamllint,
+    -- diagnostics.ansiblelint,
+	},
 })
