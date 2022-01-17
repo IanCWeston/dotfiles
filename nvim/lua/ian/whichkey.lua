9fdea7ba8254e7589fde559099d1d44ff20b69b0
Ian Weston <iancweston@gmail.com>
2022-01-16 23:47:19 -0500

    add trouble

--- nvim/lua/ian/whichkey.lua
+++ nvim/lua/ian/whichkey.lua
@@ -159,6 +159,7 @@ local mappings = {
       "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
       "Workspace Symbols",
     },
+    t = { "<cmd>TroubleToggle<cr>", "Trouble" },
   },
   s = {
     name = "Search",
