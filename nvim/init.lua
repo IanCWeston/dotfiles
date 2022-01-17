eaf758e60464951d25517a0a89d15736bb664c84
Ian Weston <iancweston@gmail.com>
2022-01-16 23:10:21 -0500

    better layout of init

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
--- Base
+-- BASE
 require("ian.options")
 require("ian.keymaps")
 require("ian.plugins")
@@ -7,19 +7,25 @@ require("ian.colorscheme")
 -- LSP
 require("ian.cmp")
 require("ian.lsp")
+require("ian.lsp.null-ls")
+
+-- CODE HELPS
 require("ian.telescope")
 require("ian.treesitter")
 require("ian.autopairs")
 require("ian.comment")
 require("ian.gitsigns")
+require("ian.indentline")
+require("ian.project")
+
+-- VISUAL ADD-ONS
 require("ian.nvim-tree")
 require("ian.bufferline")
-require("ian.lsp.null-ls")
 require("ian.lualine")
 require("ian.toggleterm")
-require("ian.project")
-require("ian.impatient")
-require("ian.indentline")
 require("ian.alpha")
+
+-- EXTRAS
+require("ian.impatient")
 require("ian.whichkey")
 require("ian.autocommands")
