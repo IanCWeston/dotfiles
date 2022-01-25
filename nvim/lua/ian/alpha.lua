8b248a88271169c31729833c1bd66ea1a595fc68
Ian Weston <iancweston@gmail.com>
2022-01-24 21:29:27 -0500

    more apt footer

--- nvim/lua/ian/alpha.lua
+++ nvim/lua/ian/alpha.lua
@@ -28,7 +28,7 @@ local function footer()
   -- local fortune = handle:read("*a")
   -- handle:close()
   -- return fortune
-  return "I   Neoim"
+  return "I   Efficiency"
 end
 
 dashboard.section.footer.val = footer()
@@ -38,5 +38,4 @@ dashboard.section.header.opts.hl = "Include"
 dashboard.section.buttons.opts.hl = "Keyword"
 
 dashboard.opts.opts.noautocmd = true
--- vim.cmd([[autocmd User AlphaReady echo 'ready']])
 alpha.setup(dashboard.opts)
