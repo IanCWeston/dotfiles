1999a1ad5272e3efff212a06dbc10642e2ed4a23
Ian Weston <55521563+IanCWeston@users.noreply.github.com>
2022-03-26 23:35:25 -0400

    update filetypes for ansible

---
+++ nvim/lua/ian/filetype.lua
@@ -0,0 +1,12 @@
+local filetype_status_ok, filetype = pcall(require, "filetype")
+if not filetype_status_ok then
+  return
+end
+
+require("filetype").setup({
+    overrides = {
+        extensions = {
+            -- add ansible lsp support
+            yml = "yaml.ansible"
+        }
+    }
