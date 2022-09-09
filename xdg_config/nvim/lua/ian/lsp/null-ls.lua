2fb9b02f7bc60c31d7448203b760d2e7c611982b
Ian Weston <iancweston@gmail.com>
2022-09-08 23:00:46 -0700

    configure mason and revise lsp handlers

--- xdg_config/nvim/lua/ian/lsp/null-ls.lua
+++ xdg_config/nvim/lua/ian/lsp/null-ls.lua
@@ -1,34 +1,57 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+  return
 end
 
--- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
-
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
--- local diagnostics = null_ls.builtins.diagnostics
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+local diagnostics = null_ls.builtins.diagnostics
 
-null_ls.setup({
-	debug = false,
-	sources = {
-    --  LUA
-		formatting.stylua,
-
-    --  PYTHON
-		formatting.isort,
-		formatting.black,
-		-- diagnostics.flake8,
-
-    --  SHELL
+null_ls.setup {
+  debug = false,
+  sources = {
+    formatting.prettier.with {
+      extra_filetypes = { "toml" },
+      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+    },
+    formatting.isort,
+    formatting.black.with { extra_args = { "--fast" } },
+    formatting.stylua,
     formatting.shfmt,
     formatting.shellharden,
-    -- diagnostics.shellcheck,
+    diagnostics.shellcheck,
+-- diagnostics.yamllint,
+-- diagnostics.ansiblelint,
+  },
+}
 
-    -- JSON/YAML/MARKDOWN
-		formatting.prettier,
+-- local unwrap = {
+--   method = null_ls.methods.DIAGNOSTICS,
+--   filetypes = { "rust" },
+--   generator = {
+--     fn = function(params)
+--       local diagnostics = {}
+--       -- sources have access to a params object
+--       -- containing info about the current file and editor state
+--       for i, line in ipairs(params.content) do
+--         local col, end_col = line:find "unwrap()"
+--         if col and end_col then
+--           -- null-ls fills in undefined positions
+--           -- and converts source diagnostics into the required format
+--           table.insert(diagnostics, {
+--             row = i,
+--             col = col,
+--             end_col = end_col,
+--             source = "unwrap",
+--             message = "hey " .. os.getenv("USER") .. ", don't forget to handle this" ,
+--             severity = 2,
+--           })
+--         end
+--       end
+--       return diagnostics
+--     end,
+--   },
+-- }
 
-    -- YAML
-    -- diagnostics.yamllint,
-    -- diagnostics.ansiblelint,
-	},
-})
+-- null_ls.register(unwrap)
