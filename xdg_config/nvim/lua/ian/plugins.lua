2c89fa2ce2a1ef07a240c871fb4d341e8254f28b
Ian Weston <iancweston@gmail.com>
2022-09-11 17:34:27 -0700

    add new plugins

--- xdg_config/nvim/lua/ian/plugins.lua
+++ xdg_config/nvim/lua/ian/plugins.lua
@@ -1,7 +1,7 @@
-local is_wsl = (function() 
-  local output = vim.fn.systemlist "uname -r" 
+local is_wsl = (function()
+  local output = vim.fn.systemlist "uname -r"
   return not not string.find(output[1] or "", "WSL")
-end)() 
+end)()
 
 local is_mac = vim.fn.has("macunix") == 1
 
@@ -9,9 +9,9 @@ local is_linux = not is_wsl and not is_mac
 
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
     vim.cmd [[packadd packer.nvim]]
     return true
   end
@@ -38,50 +38,58 @@ packer.init({
 })
 
 return packer.startup(function(use)
-  use "wbthomason/packer.nvim" -- Have packer manage itself
+  use "wbthomason/packer.nvim"
 
   -- Prerequisites
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "nvim-lua/plenary.nvim"
+  use "nvim-lua/popup.nvim"
   use "kyazdani42/nvim-web-devicons"
 
-  use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
-  use "numToStr/Comment.nvim" -- Easily comment stuff
+  -- Tabline
   --use("akinsho/bufferline.nvim") -- Tabline
-  use "nvim-lualine/lualine.nvim" -- Status Line
-  --use("akinsho/toggleterm.nvim") -- Integrated terminal
-  use "ahmedkhalf/project.nvim" -- Project manager
-  use "lukas-reineke/indent-blankline.nvim" -- Adds indent lines
-  use "goolord/alpha-nvim" -- Start screen
-  use "folke/which-key.nvim" -- Keymap support
+
+  -- Statusline
+  use "nvim-lualine/lualine.nvim"
+
+  -- Indents
+  use "lukas-reineke/indent-blankline.nvim"
+
+  -- Startup
+  use "goolord/alpha-nvim"
 
   -- Colorschemes
   use "rebelot/kanagawa.nvim"
 
   -- Completion
   use "hrsh7th/nvim-cmp"
-  use "hrsh7th/cmp-buffer" -- buffer completions
-  use "hrsh7th/cmp-path" -- path completions
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
-  use "hrsh7th/cmp-nvim-lsp" -- nvim LSP completions
-  use "hrsh7th/cmp-nvim-lua" -- nvim lua API completions
+  use "hrsh7th/cmp-buffer"
+  use "hrsh7th/cmp-path"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-nvim-lua"
+  use "saadparwaiz1/cmp_luasnip"
 
   -- Snippets
   use "L3MON4D3/LuaSnip"
-  use "rafamadriz/friendly-snippets" -- main set of snippets
-  use "bammab/vscode-snippets-for-ansible" -- additional ansible snippets
+  use "rafamadriz/friendly-snippets"
+  use "bammab/vscode-snippets-for-ansible"
 
   -- LSP
-  use "neovim/nvim-lspconfig" -- enable LSP
-  --use("williamboman/nvim-lsp-installer") -- language server installer
-  use "williamboman/mason.nvim" -- manage external editor tooling i.e lsp servers
-  use "williamboman/mason-lspconfig.nvim" -- automatically install language servers to stdpath
+  use "neovim/nvim-lspconfig"
+  use "williamboman/mason.nvim"
+  use "williamboman/mason-lspconfig.nvim"
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json format
-  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+  use "jose-elias-alvarez/null-ls.nvim"
+  use "folke/trouble.nvim"
+  use "j-hui/fidget.nvim"
 
-  -- Telescope / Fuzzy Finder
+  -- Telescope
   use "nvim-telescope/telescope.nvim"
   use { "nvim-telescope/telescope-fzf-native.nvim", run = "make" }
-  use { "nvim-telescope/telescope-file-browser.nvim" }
+  use "nvim-telescope/telescope-file-browser.nvim"
+  use "nvim-telescope/telescope-ui-select.nvim"
+
+  -- Projects
+  use "ahmedkhalf/project.nvim"
 
   -- Treesitter
   use "nvim-treesitter/nvim-treesitter"
@@ -90,11 +98,49 @@ return packer.startup(function(use)
 
   -- Git
   use "lewis6991/gitsigns.nvim"
+  use "sindrets/diffview.nvim"
+
+  -- Editting support
+  use "abecodes/tabout.nvim"
+  use "monaqa/dial.nvim"
+  use "kylechui/nvim-surround"
+  use "windwp/nvim-autopairs"
+
+  -- Comments
+  use "numToStr/Comment.nvim"
+
+  -- Find and replace
+  use "windwp/nvim-spectre"
+
+  -- Keybinds
+  use "folke/which-key.nvim"
+
+  -- Explorer (currenting A/Bing two options)
+  use "tamago324/lir.nvim"
+  use "tamago324/lir-git-status.nvim"
+
+  use { "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = { "MunifTanjim/nui.nvim" }
+  }
+
+  -- Movement
+  use "ggandor/leap.nvim"
+  use "ThePrimeagen/harpoon"
+
+  -- Session
+  use "rmagatti/auto-session"
+  use "rmagatti/session-lens"
+
+  -- Quickfix
+  use "kevinhwang91/nvim-bqf"
 
   -- Extras
+  use "lewis6991/impatient.nvim"
   use "nathom/filetype.nvim"
-  use "folke/trouble.nvim"
-  use "abecodes/tabout.nvim"
+  use "rcarriga/nvim-notify"
+  use "Vhyrro/neorg"
+
 
   use { "norcalli/nvim-colorizer.lua",
     config = function()
