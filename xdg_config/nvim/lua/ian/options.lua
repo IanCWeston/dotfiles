45ca8001feafa5fb5578d57166dd05e73113161c
Ian Weston <iancweston@gmail.com>
2022-10-14 22:50:59 -0400

    update bar to account for no bufferline

--- xdg_config/nvim/lua/ian/options.lua
+++ xdg_config/nvim/lua/ian/options.lua
@@ -3,32 +3,32 @@ local options = {
   clipboard = "unnamedplus", -- allows neovim to access the system clipboard
   completeopt = { "menuone", "noselect" }, -- mostly just for cmp
   conceallevel = 0, -- so that `` is visible in markdown files
+  cursorline = true, -- highlight the current line
+  expandtab = true, -- convert tabs to spaces
   fileencoding = "utf-8", -- the encoding written to a file
-  hlsearch = true, -- highlight all matches on previous search pattern
-  pumheight = 10, -- pop up menu height
   ignorecase = true, -- ignore case in search patterns
+  list = true, -- allows showing hidden characters
+  mouse = "a", -- allow the mouse to be used in neovim
+  number = true, -- set numbered lines
+  pumheight = 10, -- pop up menu height
+  relativenumber = false, -- set relative numbered lines
+  scrolloff = 8, -- is one of my fav
+  shiftwidth = 2, -- the number of spaces inserted for each indentation
+  showtabline = 0, -- don't show the tabline
+  sidescrolloff = 8,
+  signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
   smartcase = true,
   smartindent = true, -- make indenting smarter again
   splitbelow = true, -- force all horizontal splits to go below current window
   splitright = true, -- force all vertical splits to go to the right of current window
   swapfile = false, -- creates a swapfile
+  tabstop = 2, -- insert 2 spaces for a tab
+  termguicolors = true,
   timeoutlen = 500, -- time to wait for a mapped sequence to complete (in milliseconds)
   undofile = true, -- enable persistent undo
   updatetime = 250, -- faster completion (4000ms default)
-  writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  expandtab = true, -- convert tabs to spaces
-  shiftwidth = 2, -- the number of spaces inserted for each indentation
-  tabstop = 2, -- insert 2 spaces for a tab
-  number = true, -- set numbered lines
-  relativenumber = false, -- set relative numbered lines
-  cursorline = true, -- highlight the current line
   wrap = false, -- display lines as one long line
-  scrolloff = 8, -- is one of my fav
-  sidescrolloff = 8,
-  signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
-  termguicolors = true,
-  mouse = "a", -- allow the mouse to be used in neovim
-  list = true -- allows showing hidden characters
+  writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 }
 
 for k, v in pairs(options) do
@@ -38,9 +38,5 @@ end
 vim.opt.listchars:append("space:⋅")
 vim.opt.listchars:append("eol:↴")
 
-vim.cmd("set whichwrap+=<,>,[,],h,l")
-vim.cmd([[set iskeyword+=-]])
-
--- other options
--- vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
--- vim.opt.shortmess:append "c"
+-- vim.cmd("set whichwrap+=<,>,[,],h,l")
+-- vim.cmd([[set iskeyword+=-]])
