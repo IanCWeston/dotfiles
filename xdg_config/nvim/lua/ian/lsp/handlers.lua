28572914a812d98df626d3ae8744ebf9d7aeae4f
Ian Weston <iancweston#gmail.com>
2022-11-10 23:12:21 -0500

    not using typescript or java

--- xdg_config/nvim/lua/ian/lsp/handlers.lua
+++ xdg_config/nvim/lua/ian/lsp/handlers.lua
@@ -108,17 +108,6 @@ M.on_attach = function(client, bufnr)
   lsp_keymaps(bufnr)
   -- attach_navic(client, bufnr)
 
-  if client.name == "tsserver" then
-    require("lsp-inlayhints").on_attach(client, bufnr)
-  end
-
-  if client.name == "jdt.ls" then
-    vim.lsp.codelens.refresh()
-    if JAVA_DAP_ACTIVE then
-      require("jdtls").setup_dap { hotcodereplace = "auto" }
-      require("jdtls.dap").setup_dap_main_class_configs()
-    end
-  end
 end
 
 function M.enable_format_on_save()
