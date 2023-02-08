13eabbc6371cd8c17d2039fe5971d5988dd833de
Ian Weston <iancweston#gmail.com>
2023-02-07 20:24:01 -0500

    setup packer lsp-zero

--- xdg_config/nvim/lua/ian/plugins.lua
+++ xdg_config/nvim/lua/ian/plugins.lua
@@ -54,22 +54,33 @@ return packer.startup(function(use)
   -- use "rebelot/kanagawa.nvim"
 
   -- Completion
-  use "hrsh7th/nvim-cmp"
-  use "hrsh7th/cmp-buffer"
-  use "hrsh7th/cmp-path"
-  use "hrsh7th/cmp-nvim-lsp"
   use "hrsh7th/cmp-nvim-lua"
-  use "saadparwaiz1/cmp_luasnip"
-
-  -- Snippets
-  use "L3MON4D3/LuaSnip"
-  use "rafamadriz/friendly-snippets"
   use "bammab/vscode-snippets-for-ansible"
 
   -- LSP
-  use "neovim/nvim-lspconfig"
-  use "williamboman/mason.nvim"
-  use "williamboman/mason-lspconfig.nvim"
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {'williamboman/mason.nvim'},           -- Optional
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+  
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},         -- Required
+      {'hrsh7th/cmp-nvim-lsp'},     -- Required
+      {'hrsh7th/cmp-buffer'},       -- Optional
+      {'hrsh7th/cmp-path'},         -- Optional
+      {'saadparwaiz1/cmp_luasnip'}, -- Optional
+      {'hrsh7th/cmp-nvim-lua'},     -- Optional
+  
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},             -- Required
+      {'rafamadriz/friendly-snippets'}, -- Optional
+    }
+  }
+
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json format
   use "jose-elias-alvarez/null-ls.nvim"
   use "folke/trouble.nvim"
