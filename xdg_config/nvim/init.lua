bfbe8cb34db2966d50a2e563390ea829b08c1d8c
Ian Weston <iancweston#gmail.com>
2023-02-23 20:45:22 -0500

    add lazy.nvim bootstrap

--- xdg_config/nvim/init.lua
+++ xdg_config/nvim/init.lua
@@ -2,29 +2,29 @@
 require("ian.options")
 require("ian.keymaps")
 require("ian.autocommands")
-require("ian.plugins")
-require("ian.colorscheme")
+require("ian.core.lazy")
+-- require("ian.colorscheme")
 
--- LSP
-require("ian.lsp")
+-- -- LSP
+-- require("ian.lsp")
 
--- CODE HELPS
-require("ian.telescope")
-require("ian.treesitter")
-require("ian.autopairs")
-require("ian.gitsigns")
-require("ian.indentline")
-require("ian.project")
+-- -- CODE HELPS
+-- require("ian.telescope")
+-- require("ian.treesitter")
+-- require("ian.autopairs")
+-- require("ian.gitsigns")
+-- require("ian.indentline")
+-- require("ian.project")
 
--- VISUAL ADD-ONS
-require("ian.lualine")
-require("ian.alpha")
-require("ian.trouble")
+-- -- VISUAL ADD-ONS
+-- require("ian.lualine")
+-- require("ian.alpha")
+-- require("ian.trouble")
 
--- EXTRAS
-require("ian.whichkey")
-require("ian.comment")
-require("ian.colorizer")
-require("ian.leap")
-require("ian.surround")
-require("ian.neo-tree")
+-- -- EXTRAS
+-- require("ian.whichkey")
+-- require("ian.comment")
+-- require("ian.colorizer")
+-- require("ian.leap")
+-- require("ian.surround")
+-- require("ian.neo-tree")
