a94398a711024abba049b876a6c872e4b46b5843
Ian Weston <iancweston#gmail.com>
2023-02-26 23:03:29 -0500

    add generic editing plugins to lazy

---
+++ xdg_config/nvim/lua/ian/plugins/editor.lua
@@ -0,0 +1,86 @@
+return {
+  -- FILE EXPLORER
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    cmd = "Neotree",
+    opts = {
+      filesystem = {
+        bind_to_cwd = false,
+      },
+      window = {
+        mappings = {
+          ["<space>"] = "none",
+          ["l"] = "open",
+          ["h"] = "close_node",
+        },
+      },
+    },
+  },
+  -- FUZZY FINDER
+  {
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    version = false, -- telescope did only one release, so use HEAD for now
+    opts = {
+      defaults = {
+        prompt_prefix = " ",
+        mappings = {
+          i = {
+            ["<c-t>"] = function(...)
+              return require("trouble.providers.telescope").open_with_trouble(...)
+            end,
+            ["<C-Down>"] = function(...)
+              return require("telescope.actions").cycle_history_next(...)
+            end,
+            ["<C-Up>"] = function(...)
+              return require("telescope.actions").cycle_history_prev(...)
+            end,
+            ["<C-f>"] = function(...)
+              return require("telescope.actions").preview_scrolling_down(...)
+            end,
+            ["<C-b>"] = function(...)
+              return require("telescope.actions").preview_scrolling_up(...)
+            end,
+          },
+          n = {
+            ["q"] = function(...)
+              return require("telescope.actions").close(...)
+            end,
+          },
+        },
+      },
+    },
+  },
+  -- Faster fuzzy finder results
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    build = "make",
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
+  },
+  -- Better diagnostic list
+  {
+    "folke/trouble.nvim",
+    cmd = { "TroubleToggle", "Trouble" },
+    opts = { use_diagnostic_signs = true },
+  },
+  -- Todo Comments
+  {
+    "folke/todo-comments.nvim",
+    cmd = { "TodoTrouble", "TodoTelescope" },
+    event = { "BufReadPost", "BufNewFile" },
+    config = true,
+  },
+  -- PROJECT MGMT
+  {
+    "ahmedkhalf/project.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    cmd = "Telescope projects",
+    config = function()
+      require("project_nvim").setup()
+      require("telescope").load_extension("projects")
+    end,
+  },
+}
