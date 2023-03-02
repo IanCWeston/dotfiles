ae65cfcc0a2bb38540ef0ffb234706afdbef8e45
Ian Weston <iancweston#gmail.com>
2023-03-02 16:19:49 -0500

    out with the old

--- xdg_config/nvim/init.lua
+++ xdg_config/nvim/init.lua
@@ -1,30 +1,5 @@
--- BASE
 require("ian.core.options")
 require("ian.core.keymaps")
 require("ian.core.autocommands")
 require("ian.core.lazy")
--- require("ian.colorscheme")
 
--- -- LSP
--- require("ian.lsp")
-
--- -- CODE HELPS
--- require("ian.telescope")
--- require("ian.treesitter")
--- require("ian.autopairs")
--- require("ian.gitsigns")
--- require("ian.indentline")
--- require("ian.project")
-
--- -- VISUAL ADD-ONS
--- require("ian.lualine")
--- require("ian.alpha")
--- require("ian.trouble")
-
--- -- EXTRAS
--- require("ian.whichkey")
--- require("ian.comment")
--- require("ian.colorizer")
--- require("ian.leap")
--- require("ian.surround")
--- require("ian.neo-tree")
