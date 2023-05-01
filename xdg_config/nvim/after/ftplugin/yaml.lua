61cd8fe7bedf71de4918ae66272918f6a01681e1
Ian Weston <iancweston#gmail.com>
2023-05-01 15:26:50 -0400

    make yaml filetype changes optional

--- xdg_config/nvim/after/ftplugin/yaml.lua
+++ xdg_config/nvim/after/ftplugin/yaml.lua
@@ -1,1 +1,11 @@
-vim.opt.filetype = "yaml.ansible"
+vim.keymap.set("n", "<leader>A", function()
+  vim.cmd("LspStop")
+
+  if vim.bo.filetype == "yaml.ansible" then
+    vim.opt.filetype = "yaml"
+  else
+    vim.opt.filetype = "yaml.ansible"
+  end
+
+  vim.cmd("LspRestart")
+end, { desc = "Ansible filetype toggle" })
