98f3bf2487813a919b7b9b68ea9e54f470fa89b8
Ian Weston <iancweston#gmail.com>
2023-08-09 08:37:06 -0400

    Revert "clean up icons from dashboard"
    This reverts commit adc3f3f7c4426c08b004327f15bcfe0929625ffa.
    
    Will stick with icons after fixing with 'nerdfix'

--- xdg_config/nvim/lua/ian/plugins/ui.lua
+++ xdg_config/nvim/lua/ian/plugins/ui.lua
@@ -16,14 +16,14 @@ return {
 
     dashboard.section.header.val = vim.split(logo, "\n")
     dashboard.section.buttons.val = {
-      dashboard.button("f", "Find file", ":Telescope find_files <CR>"),
-      dashboard.button("p", "Find project", ":Telescope projects <CR>"),
-      dashboard.button("n", "New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("r", "Recent files", ":Telescope oldfiles <CR>"),
-      dashboard.button("g", "Find text", ":Telescope live_grep <CR>"),
-      dashboard.button("c", "Config", ":e $MYVIMRC <CR>"),
-      dashboard.button("l", "Lazy", ":Lazy<CR>"),
-      dashboard.button("q", "Quit", ":qa<CR>"),
+      dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+      dashboard.button("p", " " .. " Find project", ":Telescope projects <CR>"),
+      dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+      dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+      dashboard.button("l", "鈴" .. " Lazy", ":Lazy<CR>"),
+      dashboard.button("q", " " .. " Quit", ":qa<CR>"),
     }
     for _, button in ipairs(dashboard.section.buttons.val) do
       button.opts.hl = "AlphaButtons"
@@ -54,7 +54,7 @@ return {
       callback = function()
         local stats = require("lazy").stats()
         local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-        dashboard.section.footer.val = "Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+        dashboard.section.footer.val = "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
         pcall(vim.cmd.AlphaRedraw)
       end,
     })
