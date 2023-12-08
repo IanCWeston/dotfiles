45e2425878d2faf90a88c3dd66f7a623fdff68e4
Ian Weston <55521563+IanCWeston@users.noreply.github.com>
2023-12-08 16:58:58 -0500

    switch to a more gentle colorscheme

--- xdg_config/nvim/lua/ian/plugins/colorscheme.lua
+++ xdg_config/nvim/lua/ian/plugins/colorscheme.lua
@@ -1,10 +1,48 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    lazy = false,
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
+    opts = {
+      flavor = "mocha",
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
     config = function()
-      vim.cmd.colorscheme("tokyonight")
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
 }
