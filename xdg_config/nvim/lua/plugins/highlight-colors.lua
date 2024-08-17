1afa2e798baa4192a87e4943a6791bace675e820
Ian Weston <55521563+IanCWeston@users.noreply.github.com>
2024-08-16 22:53:40 -0400

    add color highlight plugin

---
+++ xdg_config/nvim/lua/plugins/highlight-colors.lua
@@ -0,0 +1,18 @@
+return {
+  "brenoprata10/nvim-highlight-colors",
+  opts = {
+    exclude_filetypes = {
+      "alpha",
+      "dashboard",
+      "fzf",
+      "help",
+      "lazy",
+      "mason",
+      "neo-tree",
+      "notify",
+      "toggleterm",
+      "Trouble",
+      "trouble",
+    },
+  },
+}
