9b3a79c062a7da0428d316858dcb47b4edc80936
Ian Weston <iancweston#gmail.com>
2025-01-10 15:19:33 -0500

    update formatting plugin

--- xdg_config/nvim/lua/plugins/formatting.lua
+++ xdg_config/nvim/lua/plugins/formatting.lua
@@ -6,7 +6,7 @@ return {
     {
       "<leader>lf",
       function()
-        require("conform").format({ async = true, lsp_fallback = true })
+        require("conform").format({ async = true, lsp_format = "fallback" })
       end,
       mode = "",
       desc = "Format buffer",
@@ -15,20 +15,9 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
-      -- python = { "isort", "black" },
       -- javascript = { { "prettierd", "prettier" } },
     },
     -- Set up format-on-save
-    format_on_save = { timeout_ms = 500, lsp_fallback = true },
-    -- Customize formatters
-    formatters = {
-      shfmt = {
-        prepend_args = { "-i", "2" },
-      },
-    },
+    format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
   },
-  -- init = function()
-  --   -- If you want the formatexpr, here is the place to set it
-  --   vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  -- end,
 }
