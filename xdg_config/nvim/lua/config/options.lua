1b9e376de6111d5d7766cb3d3e105c52c7336a28
Ian Weston <iancweston#gmail.com>
2025-01-09 22:17:30 -0500

    make options clearer and remove existing defaults

--- xdg_config/nvim/lua/config/options.lua
+++ xdg_config/nvim/lua/config/options.lua
@@ -1,43 +1,89 @@
 local options = {
-  backup = false, -- creates a backup file
-  -- clipboard = "unnamedplus", -- allows neovim to access the system clipboard
-  cmdheight = 0, -- hide command line when not in use (requires v0.8+)
-  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-  conceallevel = 0, -- so that `` is visible in markdown files
-  cursorline = true, -- highlight the current line
-  expandtab = true, -- convert tabs to spaces
-  fileencoding = "utf-8", -- the encoding written to a file
-  ignorecase = true, -- ignore case in search patterns
-  laststatus = 3, -- set global statusline (requires v0.7+)
-  list = true, -- allows showing hidden characters
-  mouse = "a", -- allow the mouse to be used in neovim
-  number = true, -- set numbered lines
-  pumheight = 10, -- pop up menu height
-  relativenumber = false, -- set relative numbered lines
-  scrolloff = 8, -- is one of my fav
-  shiftwidth = 2, -- the number of spaces inserted for each indentation
-  showtabline = 0, -- don't show the tabline
-  sidescrolloff = 8,
-  signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
+  -- Sync OS clipboard with neovim
+  -- clipboard = "unnamedplus",
+
+  -- Make line numbers default
+  number = true,
+  -- Set relative numbered lines
+  -- relativenumber = true,
+
+  -- Hide command line when not in use
+  cmdheight = 0,
+
+  -- Don't show mode since it's alredy in status line
+  showmode = false,
+
+  -- Show which line your cursor is on
+  cursorline = true,
+
+  -- Convert tabs to spaces
+  expandtab = true,
+
+  -- Set the encoding written to a file
+  fileencoding = "utf-8",
+
+  -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+  ignorecase = true,
   smartcase = true,
-  smartindent = true, -- make indenting smarter again
-  splitbelow = true, -- force all horizontal splits to go below current window
-  splitright = true, -- force all vertical splits to go to the right of current window
-  swapfile = false, -- creates a swapfile
-  tabstop = 2, -- insert 2 spaces for a tab
-  termguicolors = true,
-  timeoutlen = 500, -- time to wait for a mapped sequence to complete (in milliseconds)
-  undofile = true, -- enable persistent undo
-  updatetime = 250, -- faster completion (4000ms default)
-  wrap = false, -- display lines as one long line
-  writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+
+  -- Set global statusline
+  laststatus = 3,
+
+  -- Sets how neovim will display certain whitespace characters in the editor
+  list = true,
+
+  -- Allow the mouse to be used in neovim
+  mouse = "a",
+
+  -- Pop up menu height
+  pumheight = 10,
+
+  -- Minimal number of screen lines to keep above and below the cursor
+  scrolloff = 10,
+  -- Same for side of cursor
+  sidescrolloff = 10,
+
+  -- Default number of spaces inserted for each indentation
+  -- Insert 2 spaces for a tab
+  shiftwidth = 2,
+  tabstop = 2,
+
+  -- Don't show the tabline
+  --showtabline = 0,
+
+  -- Keep signcolumn on by default
+  signcolumn = "yes",
+
+  -- Enable break indent
+  breakindent = true,
+
+  -- Smart indenting when starting a new line
+  -- See `:help 'smartindent'`
+  smartindent = true,
+
+  -- Configure how splits should be opened
+  splitright = true,
+  splitbelow = true,
+
+  -- Don't create swapfiles
+  swapfile = false,
+
+  -- Decrease update time
+  updatetime = 250,
+
+  -- Decrease mapped sequence wait time
+  timeoutlen = 300,
+
+  -- Enable persistent undo
+  undofile = true,
+
+  -- Display lines as one long line
+  wrap = false,
 }
 
 for k, v in pairs(options) do
   vim.opt[k] = v
 end
 
+--vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 -- vim.opt.listchars:append("eol:↴")
-
--- vim.cmd("set whichwrap+=<,>,[,],h,l")
--- vim.cmd([[set iskeyword+=-]])
