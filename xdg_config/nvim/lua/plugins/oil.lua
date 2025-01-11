60f27ed251bea48a3ac084e3362c616ef4c431cc
Ian Weston <55521563+IanCWeston@users.noreply.github.com>
2025-01-10 22:13:08 -0500

    better oil setup

--- xdg_config/nvim/lua/plugins/oil.lua
+++ xdg_config/nvim/lua/plugins/oil.lua
@@ -1,9 +1,18 @@
 return {
   "stevearc/oil.nvim",
   dependencies = { "echasnovski/mini.icons" },
+  keys = {
+    { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+  },
+  ---@module 'oil'
+  ---@type oil.SetupOpts
   opts = {
+    skip_confirm_for_simple_edits = true,
     keymaps = {
-      ["<leader>e"] = "actions.close",
+      -- WARN: Blocks macros in Oil buffer
+      ["q"] = { "actions.close", mode = "n" },
+      ["h"] = { "actions.parent", mode = "n" },
+      ["l"] = { "actions.select", mode = "n" },
     },
   },
 }
