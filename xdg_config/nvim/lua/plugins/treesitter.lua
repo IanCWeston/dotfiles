28817c889d43219f986bb4902e789743a10f0e00
Ian Weston <iancweston#gmail.com>
2025-01-12 23:18:39 -0500

    more modern treesitter config

--- xdg_config/nvim/lua/plugins/treesitter.lua
+++ xdg_config/nvim/lua/plugins/treesitter.lua
@@ -4,90 +4,68 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
     version = false, -- last release is way too old and doesn't work on Windows
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
+    event = { "VeryLazy" },
+    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+    keys = {
+      { "v", desc = "Increment Selection" },
+      { "V", desc = "Decrement Selection", mode = "x" },
+    },
+    opts_extend = { "ensure_installed" },
+    ---@type TSConfig
+    ---@diagnostic disable-next-line: missing-fields
     opts = {
-      highlight = {
-        enable = true,
-        disable = function(lang, buf)
-          local max_filesize = 1000 * 1024 -- 1 MB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-        end,
-        additional_vim_regex_highlighting = true,
-      },
-      indent = {
-        enable = true,
-        disable = { "yaml", "python" },
+      highlight = { enable = true },
+      indent = { enable = true },
+      ensure_installed = {
+        "bash",
+        "c",
+        "diff",
+        "dockerfile",
+        "go",
+        "html",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "printf",
+        "python",
+        "query",
+        "regex",
+        "toml",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
       },
       incremental_selection = {
         enable = true,
         keymaps = {
+          init_selection = "v",
           node_incremental = "v",
+          scope_incremental = false,
           node_decremental = "V",
         },
       },
-      autopairs = {
-        enable = true,
-      },
-      context_commentstring = { enable = true, enable_autocmd = false },
-      ensure_installed = {
-        "bash",
-        "dockerfile",
-        "go",
-        "vimdoc",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "regex",
-        "vim",
-        "yaml",
-      },
       textobjects = {
-        select = {
-          enable = true,
-          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ["aa"] = "@parameter.outer",
-            ["ia"] = "@parameter.inner",
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-          },
-        },
         move = {
           enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            ["]]"] = "@function.outer",
-          },
-          goto_next_end = {
-            ["]m"] = "@function.outer",
-          },
-          goto_previous_start = {
-            ["[["] = "@function.outer",
-          },
-          goto_previous_end = {
-            ["[m"] = "@function.outer",
-          },
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ["]p"] = "@parameter.inner",
-          },
-          swap_previous = {
-            ["[p"] = "@parameter.inner",
-          },
+          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
         },
       },
     },
+    ---@param opts TSConfig
     config = function(_, opts)
+      -- TODO: Reivew this code if it's needed
+      -- if type(opts.ensure_installed) == "table" then
+      --   opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
+      -- end
       require("nvim-treesitter.configs").setup(opts)
     end,
   },
