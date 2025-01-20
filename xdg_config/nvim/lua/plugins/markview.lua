d0e818cfcc07021332a232b0d033d16edf8fa4f3
Ian Weston <iancweston#gmail.com>
2025-01-19 22:40:26 -0500

    markview is much more powerful than I originally thought

--- xdg_config/nvim/lua/plugins/markview.lua
+++ xdg_config/nvim/lua/plugins/markview.lua
@@ -5,5 +5,18 @@ return {
   dependencies = {
     "echasnovski/mini.icons",
   },
-  opts = {},
+  opts = function()
+    local presets = require("markview.presets")
+    return {
+      headings = presets.headings.glow,
+      horizontal_rules = presets.horizontal_rules.thin,
+    }
+  end,
+  config = function(_, opts)
+    require("markview").setup(opts)
+
+    -- Extras
+    require("markview.extras.headings").setup()
+    require("markview.extras.checkboxes").setup()
+  end,
 }
