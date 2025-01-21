3a4a53a15d7c8294eeef546066f9ee18cc592da6
Ian Weston <iancweston#gmail.com>
2025-01-20 21:33:45 -0500

    clearer file names

--- xdg_config/nvim/lua/plugins/treesitter.lua
+++ xdg_config/nvim/lua/plugins/treesitter.lua
@@ -69,4 +69,23 @@ return {
       require("nvim-treesitter.configs").setup(opts)
     end,
   },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "VeryLazy",
+    keys = {
+      {
+        "[c",
+        function()
+          require("treesitter-context").go_to_context(vim.v.count1)
+        end,
+        desc = "Jump to context",
+      },
+      {
+        "<leader>uc",
+        "<cmd>TSContextToggle<cr>",
+        desc = "Toggle TS Context",
+      },
+    },
+    opts = {},
+  },
 }
