c8131e1e7a9113136e991f44826eab9d0a2c6ad3
Ian Weston <55521563+IanCWeston@users.noreply.github.com>
2025-04-27 15:10:43 -0400

    chore: migrate lsp features

--- xdg_config/nvim/lua/config/lsp.lua
+++ xdg_config/nvim/lua/config/lsp.lua
@@ -1,12 +1,3 @@
--- local lsp_configs = {}
---
--- for _, f in pairs(vim.api.nvim_get_runtime_file("lsp/*.lua", true)) do
---   local server_name = vim.fn.fnamemodify(f, ":t:r")
---   table.insert(lsp_configs, server_name)
--- end
-
--- vim.lsp.enable(lsp_configs)
-
 -- NOTE: Choosing to specify each server manually for now
 -- for better management
 vim.lsp.enable({
@@ -59,7 +50,6 @@ vim.lsp.enable({
   -- "tailwindcss",
 })
 
-
 -- vim.api.nvim_create_autocmd('LspAttach', {
 --   callback = function(args)
 --     -- Unset 'formatexpr'
@@ -71,23 +61,81 @@ vim.lsp.enable({
 --   end,
 -- })
 
--- -- Prefer LSP folding if client supports it
--- vim.api.nvim_create_autocmd('LspAttach', {
---     callback = function(args)
---          local client = vim.lsp.get_client_by_id(args.data.client_id)
---          if client:supports_method('textDocument/foldingRange') then
---              local win = vim.api.nvim_get_current_win()
---              vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
---         end
---     end,
---  })
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    -- Prefer LSP folding if client supports it
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client:supports_method("textDocument/foldingRange") then
+      local win = vim.api.nvim_get_current_win()
+      vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+    end
 
--- TODO: Create an LspRestart command
--- - Q: How to force-reload LSP?
--- - A: Stop all clients, then reload the buffer. >vim
---      :lua vim.lsp.stop_client(vim.lsp.get_clients())
---      :edit
+    -- local capabilities = vim.lsp.protocol.make_client_capabilities()
+    -- capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
--- TODO: Create an LspInfo command
--- 5. Check that LSP is active ("attached") for the buffer: >vim
---     :checkhealth vim.lsp
+    -- Set LSP Keymaps
+    local map = vim.keymap.set
+    local opts = { buffer = args.buf, remap = false }
+
+    -- stylua: ignore start
+    -- map("n", "K", function() vim.lsp.buf.hover() end, opts)
+    map("n", "[d", function() vim.diagnostic.jump({count=1, float=true}) end, opts)
+    map("n", "]d", function() vim.diagnostic.jump({count=-1, float=true}) end, opts)
+    map("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    map("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+    map("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+    map("n", "gl", function() vim.diagnostic.open_float() end, opts)
+    map("n", "go", function() vim.lsp.buf.type_definition() end, opts)
+    map("n", "gr", function() vim.lsp.buf.references() end, opts)
+    map("n", "gS", function() vim.lsp.buf.signature_help() end, opts)
+    -- map("n", "<M-a>", function() vim.lsp.buf.code_action() end, opts) -- replaced with gra
+    -- stylua: ignore end
+  end,
+})
+
+-- Add borders to lsp windows
+local hover = vim.lsp.buf.hover
+---@diagnostic disable-next-line: duplicate-set-field
+vim.lsp.buf.hover = function()
+  return hover({
+    max_width = 100,
+    max_height = 14,
+    border = "rounded",
+  })
+end
+
+vim.diagnostic.config({
+  severity_sort = true,
+  float = { border = "rounded", source = "if_many" },
+  underline = { severity = vim.diagnostic.severity.ERROR },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "󰅚 ",
+      [vim.diagnostic.severity.WARN] = "󰀪 ",
+      [vim.diagnostic.severity.INFO] = "󰋽 ",
+      [vim.diagnostic.severity.HINT] = "󰌶 ",
+    },
+  },
+  virtual_text = {
+    source = "if_many",
+    spacing = 2,
+    format = function(diagnostic)
+      local diagnostic_message = {
+        [vim.diagnostic.severity.ERROR] = diagnostic.message,
+        [vim.diagnostic.severity.WARN] = diagnostic.message,
+        [vim.diagnostic.severity.INFO] = diagnostic.message,
+        [vim.diagnostic.severity.HINT] = diagnostic.message,
+      }
+      return diagnostic_message[diagnostic.severity]
+    end,
+  },
+})
+
+vim.api.nvim_create_user_command("LspRestart", function()
+  vim.lsp.stop_client(vim.lsp.get_clients())
+  vim.cmd.edit()
+end, {})
+
+vim.api.nvim_create_user_command("LspInfo", function()
+  vim.cmd.checkhealth("vim.lsp")
+end, {})
--- xdg_config/nvim/lua/plugins/lsp.lua
+++ xdg_config/nvim/lua/plugins/lsp.lua
@@ -2,8 +2,14 @@ return {
   {
     "williamboman/mason.nvim",
     cmd = { "Mason", "MasonInstall", "MasonUpdate" },
-    lazy = true,
+    lazy = false,
     config = true,
+    init = function()
+      -- Make mason packages available before loading it; allows to lazy-load mason.
+      vim.env.PATH = vim.fn.stdpath("data") .. "/mason/bin:" .. vim.env.PATH
+      -- Do not crowd home directory with NPM cache folder
+      vim.env.npm_config_cache = vim.env.HOME .. "/.cache/npm"
+    end,
   },
 
   {
