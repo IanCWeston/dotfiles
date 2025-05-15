83b385ed2819b59910be5206df81a778c2dc6172
Ian Weston <iancweston#gmail.com>
2025-05-15 08:35:45 -0400

    chore: migrate plugin keymaps to lazyspec

--- xdg_config/nvim/lua/plugins/gitsigns.lua
+++ xdg_config/nvim/lua/plugins/gitsigns.lua
@@ -1,16 +1,25 @@
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-      signs = {
-        add = { text = "▎" },
-        change = { text = "▎" },
-        delete = { text = "󰐊" },
-        topdelete = { text = "󰐊" },
-        changedelete = { text = "▎" },
-        untracked = { text = "▎" },
-      },
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  -- stylua: ignore
+  keys = {
+    { "<leader>gR", function() require("gitsigns").reset_buffer() end, desc = "Reset Buffer", },
+    { "<leader>gd", "<cmd>Gitsigns diffthis HEAD<cr>", desc = "Diff" },
+    { "<leader>gj", function() require("gitsigns").nav_hunk("next") end, desc = "Next Hunk", },
+    { "<leader>gk", function() require("gitsigns").nav_hunk("prev") end, desc = "Prev Hunk", },
+    { "<leader>gl", function() require("gitsigns").blame_line() end, desc = "Blame", },
+    { "<leader>gp", function() require("gitsigns").preview_hunk() end, desc = "Preview Hunk", },
+    { "<leader>gr", function() require("gitsigns").reset_hunk() end, desc = "Reset Hunk", },
+    { "<leader>gs", function() require("gitsigns").stage_hunk() end, desc = "Toggle Stage Hunk", },
+  },
+  opts = {
+    signs = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "󰐊" },
+      topdelete = { text = "󰐊" },
+      changedelete = { text = "▎" },
+      untracked = { text = "▎" },
     },
   },
 }
