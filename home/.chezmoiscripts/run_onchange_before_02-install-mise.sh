0714adb49e739f4bbd797d48bb8ff1c758ecc54d
Ian Weston <iancweston#gmail.com>
2025-06-03 23:22:55 -0400

    fix: ensure packages are installed before dotfile for compatibility

--- home/.chezmoiscripts/run_onchange_before_02-install-mise.sh
+++ home/.chezmoiscripts/run_onchange_before_02-install-mise.sh
@@ -4,11 +4,8 @@ set -eu
 
 echo ""
 echo "--- Installing mise ---"
-if command -v mise &> /dev/null
-then
+if command -v mise &>/dev/null; then
   echo "Mise is installed"
 else
   curl https://mise.run | sh
-  ${HOME}/.local/bin/mise install
 fi
-
