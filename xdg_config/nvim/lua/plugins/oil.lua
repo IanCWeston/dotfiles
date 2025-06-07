1729b3a9f59c080a778978d174846195d93dbd7d
Ian Weston <iancweston#gmail.com>
2025-06-06 22:59:04 -0400

    feat(oil): better functionality

--- xdg_config/nvim/lua/plugins/oil.lua
+++ xdg_config/nvim/lua/plugins/oil.lua
@@ -3,18 +3,24 @@ return {
   dependencies = { "echasnovski/mini.icons" },
   lazy = false,
   keys = {
-    { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
-    { "<leader>e", "<CMD>Oil --float<CR>", desc = "Explore filesytem" },
+    { "-", "<CMD>vsplit | Oil<CR>", desc = "Oil split" },
+    { "<leader>e", "<CMD>Oil --float<CR>", desc = "Explore filesytem (Oil)" },
   },
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {
     skip_confirm_for_simple_edits = true,
     keymaps = {
-      -- WARN: Blocks macros in Oil buffer
-      ["q"] = { "actions.close", mode = "n" },
       ["H"] = { "actions.parent", mode = "n" },
       ["L"] = { "actions.select", mode = "n" },
+    },
+    float = {
+      -- Padding around the floating window
+      padding = 2,
+      -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+      max_width = 0.8,
+      max_height = 0.8,
+      border = "rounded",
     },
   },
 }
