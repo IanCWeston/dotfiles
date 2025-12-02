865d25fbf99c9f5e1546f06d6b60c843b6672a00
Ian Weston <iancweston@gmail.com>
2025-12-01 21:09:56 -0500

    feat: add go linting

--- xdg_config/nvim/lua/plugins/linting.lua
+++ xdg_config/nvim/lua/plugins/linting.lua
@@ -5,8 +5,8 @@ return {
     -- Event to trigger linters
     events = { "BufWritePost", "BufReadPost", "InsertLeave" },
     linters_by_ft = {
-      lua = { "stylua" },
       sh = { "shellcheck" },
+      go = { "golangcilint" },
       -- Use the "*" filetype to run linters on all filetypes.
       -- ['*'] = { 'global linter' },
       -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
