1e88e31b8d17676da94f92a837acdc325683f08b
Ian Weston <iancweston#gmail.com>
2025-12-30 16:29:49 -0500

    feat: use harpoon2 over grapple
    grapple.nvim doesn't appear to be maintained any more

---
+++ xdg_config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,43 @@
+return {
+  "ThePrimeagen/harpoon",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  branch = "harpoon2",
+  opts = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
+    settings = {
+      save_on_toggle = true,
+    },
+  },
+  keys = function()
+    local keys = {
+      {
+        "<leader>m",
+        function()
+          require("harpoon"):list():add()
+        end,
+        desc = "Mark file (Harpoon)",
+      },
+      {
+        "<leader>M",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Marks/Harpoon Quick Menu",
+      },
+    }
+
+    for i = 1, 9 do
+      table.insert(keys, {
+        "<leader>" .. i,
+        function()
+          require("harpoon"):list():select(i)
+        end,
+        desc = "Harpoon to File " .. i,
+      })
+    end
+    return keys
+  end,
+}
