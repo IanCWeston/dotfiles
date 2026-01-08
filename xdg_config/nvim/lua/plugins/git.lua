08df6954eeb3e1f185ec2d4c6a278bf6da0ae883
Ian Weston <iancweston#gmail.com>
2026-01-07 21:46:12 -0500

    feat: add codediff plugin

--- xdg_config/nvim/lua/plugins/git.lua
+++ xdg_config/nvim/lua/plugins/git.lua
@@ -1,10 +1,10 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
   -- stylua: ignore
   keys = {
     { "<leader>gR", function() require("gitsigns").reset_buffer() end, desc = "Reset Buffer", },
-    { "<leader>gd", "<cmd>Gitsigns diffthis HEAD<cr>", desc = "Diff" },
     { "<leader>gj", function() require("gitsigns").nav_hunk("next") end, desc = "Next Hunk", },
     { "<leader>gk", function() require("gitsigns").nav_hunk("prev") end, desc = "Prev Hunk", },
     { "<leader>gl", function() require("gitsigns").blame_line() end, desc = "Blame", },
@@ -12,14 +12,21 @@ return {
     { "<leader>gr", function() require("gitsigns").reset_hunk() end, desc = "Reset Hunk", },
     { "<leader>gs", function() require("gitsigns").stage_hunk() end, desc = "Toggle Stage Hunk", },
   },
-  opts = {
-    signs = {
-      add = { text = "▎" },
-      change = { text = "▎" },
-      delete = { text = "󰐊" },
-      topdelete = { text = "󰐊" },
-      changedelete = { text = "▎" },
-      untracked = { text = "▎" },
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "󰐊" },
+        topdelete = { text = "󰐊" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
     },
+  },
+  {
+    "esmuellert/codediff.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    cmd = "CodeDiff",
+    keys = { { "<leader>gd", "<cmd>CodeDiff main HEAD<cr>", desc = "Diff" } },
   },
 }
