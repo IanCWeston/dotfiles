9010fb4d4f2150262a610dd51336b5d668dd7287
Ian Weston <iancweston@gmail.com>
2026-05-02 22:39:42 -0400

    fix: add configuration for duo agent

--- xdg_config/nvim/lua/plugins/sidekick.lua
+++ xdg_config/nvim/lua/plugins/sidekick.lua
@@ -7,69 +7,24 @@ return {
         backend = "tmux",
         enabled = true,
       },
-      tools = { duo = { cmd = { "duo" } } },
+      tools = {
+        duo = {
+          cmd = { "duo" },
+          native_scroll = true,
+        },
+      },
     },
   },
+  -- stylua: ignore
   keys = {
-    {
-      "<c-.>",
-      function()
-        require("sidekick.cli").focus()
-      end,
-      desc = "Sidekick Focus",
-      mode = { "n", "t", "i", "x" },
-    },
-    {
-      "<leader>aa",
-      function()
-        require("sidekick.cli").toggle()
-      end,
-      desc = "Sidekick Toggle CLI",
-    },
-    {
-      "<leader>as",
-      function()
-        require("sidekick.cli").select({ filter = { installed = true } })
-      end,
-      desc = "Select CLI",
-    },
-    {
-      "<leader>ad",
-      function()
-        require("sidekick.cli").close()
-      end,
-      desc = "Detach a CLI Session",
-    },
-    {
-      "<leader>at",
-      function()
-        require("sidekick.cli").send({ msg = "{this}" })
-      end,
-      mode = { "x", "n" },
-      desc = "Send This",
-    },
-    {
-      "<leader>af",
-      function()
-        require("sidekick.cli").send({ msg = "{file}" })
-      end,
-      desc = "Send File",
-    },
-    {
-      "<leader>av",
-      function()
-        require("sidekick.cli").send({ msg = "{selection}" })
-      end,
-      mode = { "x" },
-      desc = "Send Visual Selection",
-    },
-    {
-      "<leader>ap",
-      function()
-        require("sidekick.cli").prompt()
-      end,
-      mode = { "n", "x" },
-      desc = "Sidekick Select Prompt",
-    },
+    { "<c-.>", function() require("sidekick.cli").focus() end, desc = "Sidekick Focus", mode = { "n", "t", "i", "x" }, },
+    { "<leader>aa", function() require("sidekick.cli").toggle() end, desc = "Sidekick Toggle CLI", },
+    { "<leader>as", function() require("sidekick.cli").select({ filter = { installed = true } }) end, desc = "Select CLI", },
+    { "<leader>ad", function() require("sidekick.cli").close() end, desc = "Detach a CLI Session", },
+    { "<leader>aD", function() require("sidekick.cli").toggle({ name = "duo", focus = true }) end, desc = "Sidekick Toggle Duo", },
+    { "<leader>at", function() require("sidekick.cli").send({ msg = "{this}" }) end, mode = { "x", "n" }, desc = "Send This", },
+    { "<leader>af", function() require("sidekick.cli").send({ msg = "{file}" }) end, desc = "Send File", },
+    { "<leader>av", function() require("sidekick.cli").send({ msg = "{selection}" }) end, mode = { "x" }, desc = "Send Visual Selection", },
+    { "<leader>ap", function() require("sidekick.cli").prompt() end, mode = { "n", "x" }, desc = "Sidekick Select Prompt", },
   },
 }
