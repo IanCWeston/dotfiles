67ea506741e4385bf361165cb7e6788e4ff946dd
Ian Weston <iancweston@gmail.com>
2026-05-17 10:02:06 -0400

    fix: update sidekick config

--- xdg_config/nvim/lua/plugins/sidekick.lua
+++ xdg_config/nvim/lua/plugins/sidekick.lua
@@ -10,7 +10,6 @@ return {
       tools = {
         duo = {
           cmd = { "duo" },
-          native_scroll = true,
         },
       },
     },
@@ -18,13 +17,13 @@ return {
   -- stylua: ignore
   keys = {
     { "<c-.>", function() require("sidekick.cli").focus() end, desc = "Sidekick Focus", mode = { "n", "t", "i", "x" }, },
-    { "<leader>aa", function() require("sidekick.cli").toggle() end, desc = "Sidekick Toggle CLI", },
+    { "<leader>aa", function() require("sidekick.cli").toggle({filter = { installed = true } }) end, desc = "Sidekick Toggle CLI", },
     { "<leader>as", function() require("sidekick.cli").select({ filter = { installed = true } }) end, desc = "Select CLI", },
-    { "<leader>ad", function() require("sidekick.cli").close() end, desc = "Detach a CLI Session", },
-    { "<leader>aD", function() require("sidekick.cli").toggle({ name = "duo", focus = true }) end, desc = "Sidekick Toggle Duo", },
+    { "<leader>aD", function() require("sidekick.cli").close() end, desc = "Detach a CLI Session", },
     { "<leader>at", function() require("sidekick.cli").send({ msg = "{this}" }) end, mode = { "x", "n" }, desc = "Send This", },
     { "<leader>af", function() require("sidekick.cli").send({ msg = "{file}" }) end, desc = "Send File", },
     { "<leader>av", function() require("sidekick.cli").send({ msg = "{selection}" }) end, mode = { "x" }, desc = "Send Visual Selection", },
     { "<leader>ap", function() require("sidekick.cli").prompt() end, mode = { "n", "x" }, desc = "Sidekick Select Prompt", },
+    { "<leader>ad", function() require("sidekick.cli").toggle({ name = "duo", focus = true }) end, desc = "Sidekick Toggle Duo", },
   },
 }
