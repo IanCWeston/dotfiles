4ed60167c0b4376c6340c92aa4f27f6f5688e834
Ian Weston <55521563+IanCWeston@users.noreply.github.com>
2026-09-18 12:41:14 -0400

    feat(nvim): update plugins and keybindings

--- xdg_config/nvim/lua/plugins/git.lua
+++ xdg_config/nvim/lua/plugins/git.lua
@@ -2,16 +2,16 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
-  -- stylua: ignore
-  keys = {
-    { "<leader>gR", function() require("gitsigns").reset_buffer() end, desc = "Reset Buffer", },
-    { "<leader>gj", function() require("gitsigns").nav_hunk("next") end, desc = "Next Hunk", },
-    { "<leader>gk", function() require("gitsigns").nav_hunk("prev") end, desc = "Prev Hunk", },
-    { "<leader>gl", function() require("gitsigns").blame_line() end, desc = "Blame", },
-    { "<leader>gp", function() require("gitsigns").preview_hunk() end, desc = "Preview Hunk", },
-    { "<leader>gr", function() require("gitsigns").reset_hunk() end, desc = "Reset Hunk", },
-    { "<leader>gs", function() require("gitsigns").stage_hunk() end, desc = "Toggle Stage Hunk", },
-  },
+    -- stylua: ignore
+    keys = {
+      { "<leader>gR", function() require("gitsigns").reset_buffer() end,   desc = "Reset Buffer", },
+      { "<leader>gj", function() require("gitsigns").nav_hunk("next") end, desc = "Next Hunk", },
+      { "<leader>gk", function() require("gitsigns").nav_hunk("prev") end, desc = "Prev Hunk", },
+      { "<leader>gl", function() require("gitsigns").blame_line() end,     desc = "Blame", },
+      { "<leader>gp", function() require("gitsigns").preview_hunk() end,   desc = "Preview Hunk", },
+      { "<leader>gr", function() require("gitsigns").reset_hunk() end,     desc = "Reset Hunk", },
+      { "<leader>gS", function() require("gitsigns").stage_hunk() end,     desc = "Toggle Stage Hunk", },
+    },
     opts = {
       signs = {
         add = { text = "▎" },
